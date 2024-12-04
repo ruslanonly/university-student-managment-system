9c12adecb84f66ef026db7aa80fee228f4abec1e
ffh255 <sevafad@gmail.com>
2024-12-04 21:09:25 +0300

    Удалил refresh token из конфига.

--- go/src/internal/features/auth/config.go
+++ go/src/internal/features/auth/config.go
@@ -1,8 +1,7 @@
 package auth
 
 type Config struct {
-	Secret          string `yaml:"secret"`
-	AccessTokenTTL  int    `yaml:"access_token_ttl"`
-	RefreshTokenTTL int    `yaml:"refresh_token_ttl"`
-	RedirectURL     string `yaml:"redirect_url"`
+	Secret         string `yaml:"secret"`
+	AccessTokenTTL int    `yaml:"access_token_ttl"`
+	RedirectURL    string `yaml:"redirect_url"`
 }
