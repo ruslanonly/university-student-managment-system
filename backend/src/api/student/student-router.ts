3e6bd3079256ba708c3b0c1b00d37ba7e5d049d1
Hacheriki <hacheriki@gmail.com>
2024-12-06 00:47:11 +0300

    Cleaned repo from old backend

--- backend/src/api/student/student-router.ts
+++
@@ -1,8 +0,0 @@
-import express, { type Router } from "express";
-
-import { studentController } from "./student-сontroller";
-
-export const studentRouter: Router = express.Router();
-
-studentRouter.get("/", studentController.getStudent);
-studentRouter.get("/:id", studentController.getStudent);
