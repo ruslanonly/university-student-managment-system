7d2d85c9ca38a55ad5288fdc5b76606aa907c9a0
ruslanonly <rzaevruslanonly@gmail.com>
2024-12-07 00:04:14 +0300

    Add mongo

--- migrations/students_mongo.js
+++
@@ -1,7 +0,0 @@
-db = db.getSiblingDB('my_database');
-
-db.students.insertMany([
-    { first_name: 'John', middle_name: 'A', last_name: 'Doe' },
-    { first_name: 'Jane', middle_name: 'B', last_name: 'Smith' },
-    { first_name: 'Alice', middle_name: 'C', last_name: 'Johnson' }
-]);
